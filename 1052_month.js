5b6a08337b1660f300cbd1d2ad45955858a8a75b
Newton Lomar <newtonlomar16@gmail.com>
2022-07-16 21:36:58 -0300

    adding solution to '1052 Month' problem

---
+++ 1052_month.js
@@ -0,0 +1,23 @@
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+const entrada = Number(lines[0]);
+
+const meses = {
+  1: 'Janeiro',
+  2: 'Fevereiro',
+  3: 'Março',
+  4: 'Abril',
+  5: 'Maio',
+  6: 'Junho',
+  7: 'Julho',
+  8: 'Agosto',
+  9: 'Setembro',
+  10: 'Outubro',
+  11: 'Novembro',
+  12: 'Dezembro',
+};
+
+console.log(`${meses[entrada]}`);
